bea57628526f02165d85d80e01b41dcea2d946da
amzesmoro <amzesmoro05@gmail.com>
2020-06-12 20:20:41 +0700

    modified some codes

--- TA02/app.py
+++ TA02/app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, flash, render_template, request, redirect, url_for, send_from_directory
 from werkzeug.utils import secure_filename
+from flask import session
 import pandas as pd
 import csv
 
@@ -11,7 +12,7 @@ ALLOWED_EXTENSIONS ={ 'xlsx', 'xls', 'csv'}
 app = Flask(__name__)
 app.config["UPLOAD_PATHS"] = UPLOAD_PATHS
 
-def allowedFile(filename):
+def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route("/", methods=["GET", "POST"])    
@@ -26,55 +27,87 @@ def home():
             flash("No selected file")
             return redirect(request.url)
 
-        if file and allowedFile(file.filename):
+        if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config["UPLOAD_PATHS"], filename))
-            #return redirect(url_for("uploadedFile", filename=filename))
+            #return redirect(url_for("uploaded_file", filename=filename))
             return redirect(request.url)    
     return render_template("home.html")
 
 @app.route("/uploads/<filename>")        
-def uploadedFile(filename):
+def uploaded_file(filename):
     #return send_from_directory(app.config["UPLOAD_PATHS"], filename)
     return redirect(url_for("home"))
 
 @app.route("/holt-winters")
-def holtWinters():
+def holt_winters():
     return render_template("holt-winters.html")
 
-@app.route("/holt-winters-2")
-def holtWintersSts():
-    return render_template("holt-winters-2.html")
-
+@app.route("/holt-winters-2", methods=["POST"])
+def holt_winters_sts():
+    if request.method == "POST":
+        req = request.form
+        alpha = req["alpha"]
+        beta = req["beta"]
+        gamma = req["gamma"]
+        return render_template("holt-winters-2.html", alpha = alpha, beta = beta, gamma = gamma)
+  
 @app.route("/predict-holt-winters")
-def predictHoltWinters():
+def predict_holt_winters():
     return render_template("predict-holt-winters.html")
 
-@app.route("/svr", methods=["GET", "POST"])
+@app.route("/svr")
 def svr():
-    if request.method == "GET":
-        return render_template("svr.html")
-    elif request.method == "POST":
-        trainUni = request.form["train-uni"]
-        testUni = request.form["test-uni"]
-        trainMulti = request.form["train-multi"]
-        testMulti = request.form["test-multi"]
-        return render_template("svr-uni.html")
+    return render_template("svr.html")
 
-@app.route("/svr-uni")
-def svrUnivariate():
+@app.route("/svr-uni", methods=["POST"])
+def svr_univariate():
+    if request.method == "POST":
+        req = request.form
+        trainUni = req["train-uni"]
+        testUni = req["test-uni"]
+        # trainMulti = req["train-multi"]
+        # testMulti = req["test-multi"]
+        return render_template("svr-uni.html", trainUni = trainUni, testUni = testUni)
     return render_template("svr-uni.html")
 
-@app.route("/svr-multi")
-def svrMultivariate():
+@app.route("/svr-multi", methods=["POST"])
+def svr_multivariate():
+    if request.method == "POST":
+        req = request.form
+        trainMulti = req["train-multi"]
+        testMulti = req["test-multi"]
+        return render_template("svr-multi.html", trainMulti = trainMulti, testMulti = testMulti)
     return render_template("svr-multi.html")
 
-@app.route("/predict-svr-uni")
-def predictSvrUnivariate():
+@app.route("/predict-svr-uni", methods=["POST"])
+def predict_svr_univariate():
+    if request.method == "POST":
+        req = request.form
+        clrUni = req["clr-uni"]
+        cUni = req["c-uni"]
+        epsilonUni = req["epsilon-uni"]
+        lambdaUni = req["lambda-uni"]
+        sigmaUni = req["sigma-uni"]
+        iterationUni = req["iteration-uni"]        
+        return render_template("predict-svr-uni.html",
+        clrUni = clrUni, cUni = cUni, epsilonUni = epsilonUni, 
+        lambdaUni = lambdaUni, sigmaUni = sigmaUni, iterationUni = iterationUni)
     return render_template("predict-svr-uni.html")
 
-@app.route("/predict-svr-multi")
-def predictSvrMultivariate():
+@app.route("/predict-svr-multi", methods=["POST"])
+def predict_svr_multivariate():
+    if request.method == "POST":
+        req = request.form
+        clrMulti = req["clr-multi"]
+        cMulti = req["c-multi"]
+        epsilonMulti = req["epsilon-multi"]
+        lambdaMulti = req["lambda-multi"]
+        sigmaMulti = req["sigma-multi"]
+        iterationMulti = req["iteration-multi"]        
+        return render_template("predict-svr-multi.html",
+        clrMulti = clrMulti, cMulti = cMulti, epsilonMulti = epsilonMulti, 
+        lambdaMulti = lambdaMulti, sigmaMulti = sigmaMulti, iterationMulti = iterationMulti)
     return render_template("predict-svr-multi.html")
 
 @app.route("/about/")
